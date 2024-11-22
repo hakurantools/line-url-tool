0954e42ac2603876192f1b7090614b53ed368e07
hakurantools <tanakanaka2429@gmail.com>
2024-11-22 18:32:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ document.getElementById("urlForm").addEventListener("submit", function(event) {
     const title = encodeURIComponent(document.getElementById("title").value);
     const text = encodeURIComponent(document.getElementById("text").value);
 
-    // Denoサーバーへのリクエスト
-    fetch(`http://localhost:8000?url=${url}&img=${img}&x=${x}&y=${y}&title=${title}&text=${text}`)
+    // 新しいURL（hakurantool-line-url-to-38.deno.dev）に変更
+    fetch(`https://hakurantool-line-url-to-38.deno.dev/url?url=${url}&img=${img}&x=${x}&y=${y}&title=${title}&text=${text}`)
         .then(response => response.text())
         .then(generatedUrl => {
             document.getElementById("resultUrl").textContent = generatedUrl;
